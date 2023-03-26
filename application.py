a513930bfcd1a69d6f5356ad7adb8c6133eab3a6
Parth Arora <partharora@Parths-MacBook-Air.local>
2023-03-25 20:27:53 -0400

    details

--- application.py
+++ application.py
@@ -1,13 +1,7 @@
-import os
-import csv
-from datetime import datetime
-from werkzeug.utils import secure_filename
-
+import textwrap
 from flask import Flask, redirect, url_for, request, render_template, send_from_directory
 import serial
-import sqlite3
-from flask import g
-import time
+
 try:
     ser = serial.Serial('/dev/cu.usbmodem1301', 9600) # replace with your serial port and baud rate
 except:
@@ -17,69 +11,95 @@ except:
 
 app = Flask(__name__, static_folder="static")
 localDomain = "http://localhost:5000"
-publicDomain = "http://www.gtxr.club"
+publicDomain = ""
 DOMAIN = localDomain
-
 localPath = ""
-publicPath = "/home/GTXR/mysite/"
+publicPath = ""
 PATH = localPath
 
-# DATABASE = PATH + "static/database/database.db"
-#
-# def get_db():
-#     db = getattr(g, '_database', None)
-#     if db is None:
-#         db = g._database = sqlite3.connect(DATABASE)
-#     return db
-#
-# @app.teardown_appcontext
-# def close_connection(exception):
-#     db = getattr(g, '_database', None)
-#     if db is not None:
-#         db.close()
 
 @app.route("/")
 def openHome():
     txt = ""
     with open('book.txt') as f:
         txt = f.read()
+
+    lines = textwrap.wrap(txt, width=23)
+    output = ""
+    for i in range(0, len(lines), 23):
+        output += "\n".join(lines[i:i + 23]) + "\n"
+
+    arr = []
+    sentencesCount = [0 for i in range(0, len(output), 462)]
+    sentences = 0
+    for i in range(2, len(output)-1):
+        if output[i-1:i+1] == "<<":
+            j = i+1
+            while (output[j:j+2] != ">>"):
+                j+=1
+            output = output[:i-1] + output[i+1:j] + output[j+2:]
+            sentences += 1
+            sentencesCount[i // 462] += 1
+
+    chapters= output.split("*********")
+    arr = []
+    for chapter in chapters:
+        arr += textwrap.wrap(chapter, width=462)
+
+
+    chapters = []
+
+    # arr = []
+    #
+    # remainingText = output
+    # for i in range(5):
+    #     ind = remainingText[500:].find("=========III")
+    #     print(ind)
+    #     print(output[ind])
+    #     item = textwrap.wrap(output[:ind], width=462)
+    #     print(item)
+    #     arr += item
+    #     remainingText = output[ind:]
+
+
+    # newArr = []
+    # arr = textwrap.wrap(output, width=462)
+    # for i in range(len(arr)):
+    #     page = arr[i]
+    #     ind = page.find("=========")
+    #     if ind == -1:
+    #         newArr.append(page)
+    #     else:
+    #         newArr.append(page[:ind])
+    #         newArr.append(page[ind:])
+    # arr = newArr
+
+
+
+    # for i in range(0, len(output)):
+    #     if i % 462 == 0:
+    #         arr.append(output[i:i + 462])
+
     if ser != None:
         val = int(ser.readline().decode().strip())
         resitor = 680
         voltage = 5
         prVal = ((resitor * val) / voltage - val)
         print(prVal)
-        # return str(prVal)
-        return render_template("index.html", domain=DOMAIN, data=str(prVal), txt = txt)
+        return render_template("index.html", domain=DOMAIN, data=str(prVal), txt=arr, len = len(arr), sen = sentences, senCount = sentencesCount)
     else:
-        return render_template("index.html", domain=DOMAIN, data=str(0), txt = txt)
-
-@app.route("/source")
-def openSource():
-    val = int(ser.readline().decode().strip())
-    resitor = 680
-    voltage = 5
-    prVal = ((resitor * val) / voltage - val)
-    # prVal = str(round(prVal/10000, 2))
-    print(prVal)
-    # return str(prVal)
-    return render_template("index.html", domain=DOMAIN, data=str(prVal))
-
-# @app.route("/values", methods=["POST", "GET"])
-# def addAndShow():
-#     #Writing
-#     Name = request.form["name"]
-#     Age = request.form["age"]
-#     val = (Name, Age)
-#     sql = ''' INSERT INTO People(name,age) VALUES(?,?) '''
-#     get_db().execute(sql,val)
-#     get_db().commit()
-#     #Reading
-#     query = "SELECT * FROM People"
-#     cur = get_db().execute(query, ())
-#     rv = cur.fetchall()
-#     cur.close()
-#     return str(rv)
+        return render_template("index.html", domain=DOMAIN, data=str(0), txt=arr, len = len(arr), sen = sentences, senCount = sentencesCount)
+#
+# @app.route("/source")
+# def openSource():
+#     val = int(ser.readline().decode().strip())
+#     resitor = 680
+#     voltage = 5
+#     prVal = ((resitor * val) / voltage - val)
+#     # prVal = str(round(prVal/10000, 2))
+#     print(prVal)
+#     # return str(prVal)
+#     # return render_template("index.html", domain=DOMAIN, data=str(prVal), txt = "")
 
 if __name__ == "__main__":
     app.debug = True
